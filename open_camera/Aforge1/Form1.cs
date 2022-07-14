175f71815dc13f1f7827372021bfaab61201a48e
İkranur Güler <62421679+gulerikra@users.noreply.github.com>
2022-07-14 12:16:58 +0300

    Update Form1.cs

--- open_camera/Aforge1/Form1.cs
+++ open_camera/Aforge1/Form1.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using AForge.Video;
-using AForge.Video.DirectShow;
+using AForge.Video;   // AForge.Video kütüphanesi yüklenilir ve buraya eklenir
+using AForge.Video.DirectShow;  // AForge.Video.DirectShow kütüphanesi yüklenilir ve buraya eklenir
 
 
 namespace Aforge1
@@ -20,27 +20,27 @@ namespace Aforge1
             InitializeComponent();
         }
 
-        FilterInfoCollection kamerasayisi; // kaç adet kamera olduğunu tutar
-        VideoCaptureDevice kamera;
+        FilterInfoCollection kamerasayisi;  // kaç adet kamera olduğunu tutar
+        VideoCaptureDevice kamera;  // kullanılacak kameradan veri alabilmek için değişken
 
-
-        private void Form1_Load(object sender, EventArgs e)
+        // bilgisayara bağlı olan kameraları tarama ve comBox'a ekleme işlemi
+         private void Form1_Load(object sender, EventArgs e)
         {
-            kamerasayisi = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            kamerasayisi = new FilterInfoCollection(FilterCategory.VideoInputDevice); 
             foreach(FilterInfo f in kamerasayisi)
             {
                 comboBox1.Items.Add(f.Name);
             }
             comboBox1.SelectedIndex = 0;
         }
-
+        // butona basıldığında kamera açma işlemi
         private void button1_Click(object sender, EventArgs e)
         {
             kamera = new VideoCaptureDevice(kamerasayisi[comboBox1.SelectedIndex].MonikerString);
             kamera.NewFrame += Kamera_NewFrame;
             kamera.Start();
         }
-
+        // kullanılacak cihazın metodunu oluşturma işlemi
         private void Kamera_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
